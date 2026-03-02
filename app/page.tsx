365189e8f33b9ddcece0868b27d747e071577587
Rohan Sindhu <rohansindhu82@gmail.com>
2026-03-02 19:12:26 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+
 <About />
 export default function Home() {
   return (
